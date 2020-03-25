3e276a20791f8fbb1d67f44ca3d62b412bb49c13
sysu19351118 <61107298+sysu19351118@users.noreply.github.com>
2020-03-25 10:08:34 +0800

    Add files via upload

---
+++ 唐梓轩19351118—_作业3/student_class.h
@@ -0,0 +1,18 @@
+#include "student.h"
+#include <string>
+class ClassStudent {
+	static int const  maxStudents_ = 20;
+	static int studentCount_;
+	string code_ = "aaa2019";
+	Student* students_[maxStudents_];
+public:
+	ClassStudent(Student s[20]);
+	ClassStudent(ClassStudent&);
+	~ClassStudent() {}
+	bool AddStudent( Student* const student);
+	void ShowStudent(int id) const;
+	static void showStudentCount();
+	void ShowStudentsByIdOrder(bool (*compare)(int, int));
+};
+
+//classStudent类的定义
